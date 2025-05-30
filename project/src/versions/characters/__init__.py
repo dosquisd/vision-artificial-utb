c811df73b82d8c3eae86b54cf267da10aac921a2
JuanD <dosquisd123@gmail.com>
2025-05-29 19:40:48 -0500

    refactor: remove omnx (although I thought I did it before)

--- project/src/versions/characters/__init__.py
+++ project/src/versions/characters/__init__.py
@@ -5,11 +5,9 @@ This module imports and provides access to different versions of
 braille character detection implementations.
 """
 
-from src.versions.characters.v1.yolo import main as characterv1_yolo
-from src.versions.characters.v1.onnx import main as characterv1_onnx
+from src.versions.characters.v1 import main as characterv1
 
 
 __all__ = [
-    "characterv1_yolo",
-    "characterv1_onnx",
+    "characterv1",
 ]
--- project/src/versions/translation/__init__.py
+++ project/src/versions/translation/__init__.py
@@ -6,12 +6,10 @@ braille to text translation implementations.
 """
 
 from src.versions.translation.v1.main import main as translationv1
-from src.versions.translation.v2.yolo import main as translationv2_yolo
-from src.versions.translation.v2.onnx import main as translationv2_onnx
+from src.versions.translation.v2 import main as translationv2
 
 
 __all__ = [
     "translationv1",
-    "translationv2_yolo",
-    "translationv2_onnx",
+    "translationv2",
 ]
